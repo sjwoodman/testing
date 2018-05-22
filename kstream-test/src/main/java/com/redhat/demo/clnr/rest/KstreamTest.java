1a2f5a41ccf6cc96f1f2bbca87671582fd11b683
Simon <s.j.woodman@gmail.com>
2018-05-22 08:27:51 +0100

    inital version of non-working kstream + wildfly swam app

---
+++ kstream-test/src/main/java/com/redhat/demo/clnr/rest/KstreamTest.java
@@ -0,0 +1,32 @@
+package com.redhat.demo.clnr.rest;
+
+import org.aerogear.kafka.cdi.annotation.KafkaConfig;
+import org.aerogear.kafka.cdi.annotation.KafkaStream;
+import org.apache.kafka.streams.kstream.KStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ejb.Stateless;
+
+@Stateless
+@KafkaConfig(bootstrapServers = "#{KAFKA_SERVICE_HOST}:#{KAFKA_SERVICE_PORT}")
+public class KstreamTest {
+
+    Logger logger = LoggerFactory.getLogger(KstreamTest.class);
+
+
+    @KafkaStream(input = "ingest.api.out", output = "kstream.test.out")
+    public KStream tableTransformer(final KStream<String, String> source) {
+
+        logger.info("Initial setup");
+
+        final KStream<String, Long> successCountsPerJob = source.filter((key, value) -> {
+            System.out.println("x");
+            return value.equals("Success");
+        })
+                .groupByKey()
+                .count("successMessagesStore").toStream();
+
+        return successCountsPerJob;
+    }
+}
